ac30acab033c8de3c82f338c0c53077ccef1736a
unknown <sethsopheara@gmail.com>
2025-04-10 22:37:47 +0700

    Finish frontend setup

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
@@ -22,4 +22,66 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('front.layouts.auth-page-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'EduCore | Sign In')
+@push('stylesheets')
+    <style>
+        .wsus__sign_in {
+            margin-top: 95px;
+        }
+    </style>
+@endpush
+@section('content')
+    <section class="wsus__sign_in">
+        <div class="row align-items-center">
+            <div class="col-xxl-5 col-xl-6 col-lg-6 wow fadeInLeft">
+                <div class="wsus__sign_img">
+                    <img src="/front/images/login_img_1.jpg" alt="login" class="img-fluid">
+                    <a href="index.html">
+                        <img src="/front/images/logo.png" alt="EduCore" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-9 m-auto wow fadeInRight">
+                <div class="wsus__sign_form_area">
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                            aria-labelledby="pills-home-tab" tabindex="0">
+                            <!-- Session Status -->
+                            <x-auth-session-status class="mb-4" :status="session('status')" />
+                            <form method="POST" action="{{ route('password.email') }}"">
+                                @csrf
+                                <h2>Forgot Password?<span>!</span></h2>
+                                <p class="new_user mt-4 text-sm text-gray-600 dark:text-gray-400">
+                                    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                                </p>
+                                <div class="row">
+                                    <!-- Email Address -->
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label>Email</label>
+                                            <input id="email" type="email" name="email" :value="old('email')"
+                                                required autofocus placeholder="Enter your email here.">
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+                                    </div>
+                                    <!-- Remember Me -->
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <button type="submit" class="common_btn">
+                                                {{ __('Email Password Reset Link') }}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="back_btn" href="{{ route('home') }}">Back to Home</a>
+    </section>
+@endsection
