227a46aa2e7a81e66fd24fe4f4bffc6a1d2e9dd2
unknown <sethsopheara@gmail.com>
2025-04-10 16:02:15 +0700

    170_2_frontend_panel_mastering_home_page

--- resources/views/admin/auth/forgot-password.blade.php
+++ resources/views/admin/auth/forgot-password.blade.php
@@ -1,25 +1,48 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@extends('admin.layouts.auth-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Admin | Forgot-Password')
+@section('content')
+    <div class="container container-tight py-4">
+        <div class="text-center mb-4">
+            <a href="." class="navbar-brand navbar-brand-autodark">
+                <img src="/admin/assets/static/logo.svg" width="110" height="32" alt="Tabler" class="navbar-brand-image">
+            </a>
+        </div>
+        <form method="POST" action="{{ route('admin.password.email') }}" class="card card-md" autocomplete="off"
+            novalidate="">
+            @csrf
+            <div class="card-body">
+                <h2 class="card-title text-center mb-4">Forgot password</h2>
+                <p class="text-secondary mb-4">
+                    @if (empty(session('status')))
+                        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                    @endif
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                </p>
+                <!-- Email Address -->
+                <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control" placeholder="Enter email" id="email" name="email"
+                        value="{{ old('email') }}" required>
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+                <div class="form-footer">
+                    <button type="submit" class="btn btn-primary w-100">
+                        <!-- Download SVG icon from http://tabler-icons.io/i/mail -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                            stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z">
+                            </path>
+                            <path d="M3 7l9 6l9 -6"></path>
+                        </svg>
+                        {{ __('Email Password Reset Link') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+        <div class="text-center text-secondary mt-3">
+            Forget it, <a href="{{ route('admin.login') }}">send me back</a> to the sign in screen.
+        </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('admin.password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
--- resources/views/admin/auth/reset-password.blade.php
+++ resources/views/admin/auth/reset-password.blade.php
@@ -1,43 +1,112 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('admin.password.store') }}">
-        @csrf
-        <div class="mb-4">
-            <x-input-label for="" :value="__('Admin Reset Password Template.')" />
+@extends('admin.layouts.auth-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Admin | Forgot-Password')
+@section('content')
+    <div class="container container-tight py-4">
+        <div class="text-center mb-4">
+            <a href="." class="navbar-brand navbar-brand-autodark">
+                <img src="/admin/assets/static/logo.svg" width="110" height="32" alt="Tabler" class="navbar-brand-image">
+            </a>
         </div>
-
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)"
-                required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <form method="POST" action="{{ route('admin.password.store') }}" class="card card-md" autocomplete="off"
+            novalidate="">
+            @csrf
+            <div class="card-body">
+                <h2 class="card-title text-center mb-4">Reset your password as admin.</h2>
+                <!-- Password Reset Token -->
+                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                <!-- Email Address -->
+                <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control" placeholder="Enter email" name="email" id="email"
+                        value="{{ old('email', $request->email) }}">
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+                <!-- Password -->
+                <div class="mb-3">
+                    <label class="form-label">Password</label>
+                    <div class="input-group input-group-flat">
+                        <input type="password" class="form-control password" placeholder="Password" autocomplete="off"
+                            name="password" id="password" required aria-autocomplete="new-password">
+                        <span class="input-group-text toggle-password">
+                            <a href="javascript:;" class="link-secondary" data-bs-toggle="tooltip" aria-label="Show password"
+                                data-bs-original-title="Show password"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                    <path
+                                        d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6">
+                                    </path>
+                                </svg>
+                            </a>
+                        </span>
+                    </div>
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+                <!-- Confirm Password -->
+                <div class="mb-3">
+                    <label class="form-label">Confirm Password</label>
+                    <div class="input-group input-group-flat">
+                        <input type="password" class="form-control cf_password" placeholder="Confirm Password"
+                            autocomplete="off" name="password_confirmation" id="password_confirmation" required
+                            aria-autocomplete="new-password">
+                        <span class="input-group-text toggle-cfPassword">
+                            <a href="javascript:;" class="link-secondary" data-bs-toggle="tooltip" aria-label="Show password"
+                                data-bs-original-title="Show password"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0"></path>
+                                    <path
+                                        d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6">
+                                    </path>
+                                </svg>
+                            </a>
+                        </span>
+                    </div>
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div>
+                <div class="form-footer">
+                    <button type="submit" class="btn btn-primary w-100">
+                        {{ __('Reset Password') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+        <div class="text-center text-secondary mt-3">
+            Already have account? <a href="{{ route('admin.login') }}" tabindex="-1">Sign in</a>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+@endsection
+@push('scripts')
+    <script>
+        /**
+         * ————————————————————————————————————————————————————————————————————————————————
+         * FOR PASSWORD CHANGING TYPE WHEN CLICK
+         * ————————————————————————————————————————————————————————————————————————————————
+         */
+        $('.toggle-password').on('click', function() {
+            let iPassword = $('.password')
+            if (iPassword.attr('type') === 'password') {
+                iPassword.attr('type', 'text')
+            } else {
+                iPassword.attr('type', 'password')
+            }
+        })
+        /**
+         * ————————————————————————————————————————————————————————————————————————————————
+         * FOR PASSWORD CONFIRMATION CHANGING TYPE WHEN CLICK
+         * ————————————————————————————————————————————————————————————————————————————————
+         */
+        $('.toggle-cfPassword').on('click', function() {
+            let iConfirmPassword = $('.cf_password')
+            if (iConfirmPassword.attr('type') === 'password') {
+                iConfirmPassword.attr('type', 'text')
+            } else {
+                iConfirmPassword.attr('type', 'password')
+            }
+        })
+    </script>
+@endpush
