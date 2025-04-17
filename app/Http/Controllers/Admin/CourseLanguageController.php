a7531205598803bd9e2f094bb75bc95f2b2b9880
unknown <sethsopheara@gmail.com>
2025-04-17 19:33:29 +0700

    CRUD-COURSE-LANGUAGE/LEVEL

---
+++ app/Http/Controllers/Admin/CourseLanguageController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\CourseLanguage;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException as ValidationValidationException;
+use Str;
+
+class CourseLanguageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = [
+            'pageTitle' => 'EduCore | Course Language'
+        ];
+        $courseLanguages = CourseLanguage::paginate(15);
+        $data['courseLanguages'] = $courseLanguages;
+
+        return view('admin.pages.course.course-languages.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        $data = [
+            'pageTitle' => 'EduCore | Create Course Language'
+        ];
+        return view('admin.pages.course.course-languages.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|unique:course_languages',
+        ]);
+        $courseLanguage = new CourseLanguage();
+        $courseLanguage->name = $request->name;
+        $courseLanguage->slug = Str::slug($request->name);
+        $courseLanguage->save();
+        notyf()->success('Course Language Created Successfully');
+        return redirect()->route('admin.course-languages.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CourseLanguage $course_language)
+    {
+        return view('admin.pages.course.course-languages.edit', [
+            'pageTitle' => 'EduCore | Edit Course Language',
+            'course_language' => $course_language
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|unique:course_languages,name,' . $id,
+        ]);
+        $courseLanguage = CourseLanguage::findOrFail($id);
+        $courseLanguage->name = $request->name;
+        $courseLanguage->save();
+        notyf()->success('Course Language Updated Successfully');
+        return redirect()->route('admin.course-languages.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CourseLanguage $course_language)
+    {
+        try {
+            $course_language->delete();
+            notyf()->success('Deleted Successfully!');
+            return response()->json([
+                'message' => 'Deleted Successfully!',
+            ], 200);
+        } catch (Exception $e) {
+            logger($e);
+            return response()->json([
+                'message' => 'Something went wrong'
+            ], 500);
+        }
+    }
+}
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -13,6 +13,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="csrf_token" content="{{ csrf_token() }}" />
     <title>@yield('pageTitle')</title>
     <!-- CSS files -->
     <link href="/admin/assets/dist/css/tabler.min.css?1692870487" rel="stylesheet" />
@@ -40,7 +41,13 @@
         @include('admin.layouts.navbar')
         <div class="page-wrapper">
             <!-- Page header -->
-            @if (Route::is('admin.instructor-requests.index'))
+            @if (Route::is('admin.instructor-requests.index') ||
+                    Route::is('admin.course-languages.index') ||
+                    Route::is('admin.course-languages.create') ||
+                    Route::is('admin.course-languages.edit') ||
+                    Route::is('admin.course-levels.index') ||
+                    Route::is('admin.course-levels.create') ||
+                    Route::is('admin.course-levels.edit'))
             @else
                 @include('admin.layouts.header')
             @endif
@@ -51,6 +58,43 @@
         </div>
     </div>
     <!-- Modals -->
+    <div class="modal modal-blur fade" id="modal-danger" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-sm modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-status bg-danger"></div>
+                <div class="modal-body text-center py-4">
+                    <!-- Download SVG icon from http://tabler-icons.io/i/alert-triangle -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon mb-2 text-danger icon-lg" width="24"
+                        height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                            d="M10.24 3.957l-8.422 14.06a1.989 1.989 0 0 0 1.7 2.983h16.845a1.989 1.989 0 0 0 1.7 -2.983l-8.423 -14.06a1.989 1.989 0 0 0 -3.4 0z">
+                        </path>
+                        <path d="M12 9v4"></path>
+                        <path d="M12 17h.01"></path>
+                    </svg>
+                    <h3>Are you sure?</h3>
+                    <div class="text-secondary">Do you really want to remove this record? What you've done cannot be
+                        undone.
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="w-100">
+                        <div class="row">
+                            <div class="col"><a href="#" class="btn w-100">
+                                    Cancel
+                                </a></div>
+                            <div class="col"><a href="#" class="btn btn-danger w-100 delete-confirm-btn">
+                                    Delete
+                                </a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Libs JS -->
 
