737c6003662635e949cbf69b4358846ab055290b
unknown <sethsopheara@gmail.com>
2025-04-25 20:17:03 +0700

    257. 36_ Course Content - Working with Lesson Update (Part-3)

--- app/Models/CourseChapter.php
+++ app/Models/CourseChapter.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CourseChapter extends Model
 {
     /** @use HasFactory<\Database\Factories\CourseChapterFactory> */
     use HasFactory;
+
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(CourseChapterLessons::class, 'chapter_id', 'id');
+    }
+
+
 }
