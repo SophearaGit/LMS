d82ad6660c31b093cf0417e915a2d0c29f93e32f
unknown <sethsopheara@gmail.com>
2025-05-10 17:12:49 +0700

    314. 9_ PayPal - Enroll User

--- resources/views/front/pages/course-page.blade.php
+++ resources/views/front/pages/course-page.blade.php
@@ -659,14 +659,19 @@
                                         </a>
                                     </div>
                                     <div class="wsus__single_courses_3_footer">
-                                        <a class="common_btn" href="#">Enroll <i class="far fa-arrow-right"
-                                                aria-hidden="true"></i></a>
+                                        <a id="add_to_cart_btn_{{ $course->id }}" class="common_btn add_to_cart_btn"
+                                            data-course-id="{{ $course->id }}" href="#">Add to cart<i
+                                                class="far fa-arrow-right" aria-hidden="true"></i></a>
                                         <p>
                                             @if ($course->price == 0)
                                                 Free
                                             @else
                                                 @if ($course->discount > 0)
-                                                    <del>${{ number_format($course->price, 2) }}</del>
+                                                    <del>
+                                                        <small>
+                                                            ${{ number_format($course->price, 2) }}
+                                                        </small>
+                                                    </del>
                                                     ${{ number_format($course->price - ($course->price * $course->discount) / 100, 2) }}
                                                 @else
                                                     ${{ number_format($course->price, 2) }}
@@ -704,3 +709,53 @@
         </div>
     </section>
 @endsection
+
+@push('scripts')
+    <script>
+        // Variables.
+        const base_url = $(`meta[name="base_url"]`).attr('content');
+        const notyf = new Notyf({
+            duration: 5000,
+            dismissible: true,
+            position: {
+                x: 'right',
+                y: 'bottom',
+            },
+        });
+
+        // Reuseable functions.
+        function add_to_cart(course_id) {
+            $.ajax({
+                method: 'POST',
+                url: base_url + `/cart/${course_id}/store`,
+                data: {
+                    _token: csrf_token,
+
+                },
+                beforeSend: function() {
+                    $(`#add_to_cart_btn_${course_id}`).html('<i class="fas fa-spinner fa-spin"></i>Adding...');
+                },
+                success: function(data) {
+                    notyf.success(data.message);
+                    $(`#add_to_cart_btn_${course_id}`).html('Add to cart');
+                },
+                error: function(xhr, status, error) {
+                    let errors = xhr.responseJSON;
+                    $.each(errors, function(key, value) {
+                        notyf.error(value);
+                    });
+                    $(`#add_to_cart_btn_${course_id}`).html('Add to cart');
+                },
+            })
+        }
+
+        // On DOM load.
+        $(function() {
+            $('.add_to_cart_btn').on('click', function(e) {
+                e.preventDefault();
+                let course_id = $(this).data('course-id');
+                add_to_cart(course_id)
+            });
+        });
+    </script>
+@endpush
