13c72f68a78e96fa67dabb3001d298b71ca5129b
unknown <sethsopheara@gmail.com>
2025-08-19 12:39:30 +0700

    showing orders at student table

--- resources/views/front/pages/instructor/index.blade.php
+++ resources/views/front/pages/instructor/index.blade.php
@@ -60,24 +60,100 @@
                             </div>
                         @endif
                         <div class="col-xl-4 col-sm-6 wow fadeInUp">
-                            <div class="wsus__dash_earning">
-                                <h6>REVENUE</h6>
-                                <h3>$2456.34</h3>
-                                <p>Earning this month</p>
+                            <div class="wsus__dash_earning" style="background-color: #fbebeb;">
+                                <h6><span class="fw-semibold" style="color: #d63939;">Rejected</span> <small>Courses</small>
+                                </h6>
+                                <h3>{{ $rejectedCourses }}</h3>
                             </div>
                         </div>
                         <div class="col-xl-4 col-sm-6 wow fadeInUp">
-                            <div class="wsus__dash_earning">
-                                <h6>instructorS ENROLLMENTS</h6>
-                                <h3>16,450</h3>
-                                <p>Progress this month</p>
+                            <div class="wsus__dash_earning" style="background-color:#fef5e6;">
+                                <h6><span class="fw-semibold" style="color: #f59f00;">Pending</span> <small>Courses</small>
+                                </h6>
+                                <h3>{{ $pendingCourses }}</h3>
                             </div>
                         </div>
                         <div class="col-xl-4 col-sm-6 wow fadeInUp">
-                            <div class="wsus__dash_earning">
-                                <h6>COURSES RATING</h6>
-                                <h3>4.70</h3>
-                                <p>Rating this month</p>
+                            <div class="wsus__dash_earning" style="background-color:#eaf7ec;">
+                                <h6><span class="fw-semibold" style="color: #2fb344;">Approved</span> <small>Courses</small>
+                                </h6>
+                                <h3>{{ $approvedCourses }}</h3>
+                            </div>
+                        </div>
+                        <div class="col-xl-12 col-sm-12 wow fadeInUp">
+                            <div class="wsus__dashboard_contant">
+                                <div class="wsus__dashboard_contant_top">
+                                    <div class="wsus__dashboard_heading wow fadeInUp"
+                                        style="visibility: visible; animation-name: fadeInUp;">
+                                        <h5>Best Selling Courses</h5>
+                                    </div>
+                                </div>
+
+                                <div class="wsus__dash_course_table wow fadeInUp"
+                                    style="visibility: visible; animation-name: fadeInUp;">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="table-responsive">
+                                                <table class="table">
+                                                    <tbody>
+                                                        <tr>
+                                                            <th class="image">
+                                                                COURSES
+                                                            </th>
+                                                            <th class="details">
+
+                                                            </th>
+                                                            <th class="sale">
+                                                                SALES
+                                                            </th>
+                                                            <th class="amount">
+                                                                AMOUNT
+                                                            </th>
+                                                        </tr>
+
+                                                        @forelse ($orderItems as $item)
+                                                            <tr>
+                                                                <td class="image">
+                                                                    <div class="image_category">
+                                                                        <img src="{{ $item->course->thumbnail }}"
+                                                                            alt="{{ $item->course->title }}"
+                                                                            class="img-fluid w-100">
+                                                                    </div>
+                                                                </td>
+                                                                <td class="details">
+                                                                    <p class="rating">
+                                                                        @for ($i = 1; $i <= 5; $i++)
+                                                                            @if ($i <= round($item->course->reviews()->avg('rating')))
+                                                                                <i class="fas fa-star"></i>
+                                                                            @else
+                                                                                <i class="far fa-star"></i>
+                                                                            @endif
+                                                                        @endfor
+                                                                        <span>({{ number_format($item->course->reviews()->avg('rating') ?? 0, 1) }})</span>
+                                                                    </p>
+                                                                    <a class="title"
+                                                                        href="{{ route('courses.show', $item->course->slug) }}">{{ $item->course->title }}</a>
+                                                                </td>
+                                                                <td class="sale">
+                                                                    <p>34</p>
+                                                                </td>
+                                                                <td class="amount">
+                                                                    <p>$3,145.23</p>
+                                                                </td>
+                                                            </tr>
+                                                        @empty
+                                                            <tr>
+                                                                <td colspan="4" class="text-center">
+                                                                    No item in order.
+                                                                </td>
+                                                            </tr>
+                                                        @endforelse
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
